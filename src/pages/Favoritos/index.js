3ecbb439554791f641daff4781c33c8f1eb906f9
Fernando Junior <fernando.junior85@outlook.com>
2021-08-09 18:24:51 -0300

    Added Favoritos Page

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,9 +1,55 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
 import './favoritos.css'
 
 const Favoritos = () => {
+
+  const [filmes, setFilmes] = useState([])
+
+  useEffect(() => {
+    const minhaLista = localStorage.getItem('filmes')
+
+    setFilmes(JSON.parse(minhaLista) || [])
+
+
+  }, [])
+
+  function handleDelete (id) {
+    let filtroFilmes = filmes.filter(filme => {
+      return (filme.id !== id)
+    })
+
+    setFilmes(filtroFilmes)
+    localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+  }
+
   return (
-    <div>
-      Favoritos
+    <div className="meus-filmes">
+      <h1>Meus Filmes Favoritos</h1>
+
+      {
+        filmes.length === 0 &&
+          <span>Você não possui filmes favoritados =(</span>
+      }
+
+      <ul>
+        {
+          filmes.map(filme => {
+            return (
+              <li key={filme.id}>
+                <span>{filme.nome}</span>
+
+                <div>
+                  <Link to={`/filme/${filme.id}`}>Detalhes</Link>
+                  {/* Como a função handleDelete vai passar parametro, devemos usar uma função anonima pra nao executar automaticamente ela mas sim, somente no click */}
+                  <button onClick={ () => handleDelete(filme.id) }>Remover</button>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }

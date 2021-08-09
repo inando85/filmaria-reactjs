ef9b01c5026b19d0ecb667f38864a64501135cb4
Fernando Junior <fernando.junior85@outlook.com>
2021-08-09 14:15:39 -0300

    Added Header

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Header from './components/Header'
 import Home from './pages/Home'
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/" component={Home}/>
       </Switch>
